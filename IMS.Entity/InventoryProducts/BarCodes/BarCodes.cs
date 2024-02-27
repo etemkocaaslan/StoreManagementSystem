f7854f67b482c71f31b605a0792ad0bba95a0999
kasla <kasla@DESKTOP-SR937O1>
2024-02-26 21:31:32 -0800

    BarCodes Class Added with BarcodeId, ICollection<Orders> Orders and Barcode props

---
+++ IMS.Entity/InventoryProducts/BarCodes/BarCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace IMS.Entity.InventoryProducts
+{
+    public class BarCodes
+    {
+        public int BarCodeId { get; set; }
+        public string BarCode { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
