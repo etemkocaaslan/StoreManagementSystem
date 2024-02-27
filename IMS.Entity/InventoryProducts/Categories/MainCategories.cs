430010693bf7064ea146883638cf9fa02f086721
kasla <kasla@DESKTOP-SR937O1>
2024-02-27 07:31:09 -0800

    MainCategories Class added

---
+++ IMS.Entity/InventoryProducts/Categories/MainCategories.cs
@@ -0,0 +1,13 @@
+﻿namespace IMS.Entity.InventoryProducts
+{
+    public class MainCategories
+    {
+        public int MainCategoryId { get; set; }
+        public string MainCategoryName { get; set; }
+        public string MainCategoryDescription { get; set; }
+        public byte[] MainCategoryImage { get; set; }
+
+
+        public virtual ICollection<SecondCategories> SecondCategories{ get; set; }
+    }
+}
