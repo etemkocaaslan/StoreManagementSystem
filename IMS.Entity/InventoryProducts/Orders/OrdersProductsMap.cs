747556615c0d9b56ada354138ec6289b2feddb35
kasla <kasla@DESKTOP-SR937O1>
2024-02-27 07:33:38 -0800

    OrderProductsMap class added

---
+++ IMS.Entity/InventoryProducts/Orders/OrdersProductsMap.cs
@@ -0,0 +1,12 @@
+﻿namespace IMS.Entity.InventoryProducts
+{
+    public class OrdersProductsMap
+    {
+        public int OrderProductsCategoriesId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+
+        public virtual Orders Order { get; set; }
+        public virtual Products Product { get; set; }
+    }
+}
