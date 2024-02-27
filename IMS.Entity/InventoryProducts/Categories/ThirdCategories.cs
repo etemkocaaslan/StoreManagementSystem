312f8e4d1f2c5b5eb3585a5d5273c5ea74e8470c
kasla <kasla@DESKTOP-SR937O1>
2024-02-27 07:32:34 -0800

    ThirdCategoriess Class added

---
+++ IMS.Entity/InventoryProducts/Categories/ThirdCategories.cs
@@ -0,0 +1,17 @@
+﻿namespace IMS.Entity.InventoryProducts
+{
+    public class ThirdCategories
+    {
+        public int ThirdCategoryId { get; set; }
+        public string ThirdCategoryName { get; set; }
+        public int SecondCategoryId { get; set; }
+        public string ThirdCategoryDescription { get; set; }
+        public byte[] ThirdCategoryImage { get; set; }
+
+        //OutSide
+        public string SecondCategoryName { get; set; }
+
+        public virtual SecondCategories SecondCategory { get; set; }
+        public virtual ICollection<Vendors> Vendors { get; set; }
+    }
+}
