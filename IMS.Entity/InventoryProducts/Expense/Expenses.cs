bcab46f4d84753459f3adc89d6e09e7c3c6dbee5
kasla <kasla@DESKTOP-SR937O1>
2024-02-27 07:30:29 -0800

    Expenses Class added

---
+++ IMS.Entity/InventoryProducts/Expense/Expenses.cs
@@ -0,0 +1,10 @@
+﻿namespace IMS.Entity.InventoryProducts.Expense
+{
+    public class Expenses
+    {
+        public int      ExpenseId    {get;  set;}
+        public string   ExpenseName  { get; set; }
+        public double   ExpenseAmount{ get; set; }
+        public DateTime ExpenseDate  { get; set; }
+    }
+}
