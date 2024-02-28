863032033de29fb23f8bb66a0d09cbed80aa66e4
kasla <kasla@DESKTOP-SR937O1>
2024-02-28 07:48:04 -0800

    unnecesarry  spacing fixed

--- IMS.Entity/InventoryProducts/Expense/Expenses.cs
+++ IMS.Entity/InventoryProducts/Expense/Expenses.cs
@@ -1,10 +1,9 @@
-﻿namespace IMS.Entity.InventoryProducts.Expense
+﻿namespace IMS.Entity.InventoryProducts;
+
+public class Expenses
 {
-    public class Expenses
-    {
-        public int      ExpenseId    {get;  set;}
-        public string   ExpenseName  { get; set; }
-        public double   ExpenseAmount{ get; set; }
-        public DateTime ExpenseDate  { get; set; }
-    }
+    public int ExpenseId { get; set; }
+    public string ExpenseName { get; set; }
+    public double ExpenseAmount { get; set; }
+    public DateTime ExpenseDate { get; set; }
 }
